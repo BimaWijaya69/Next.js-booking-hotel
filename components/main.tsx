e6658e35b3634c03846eb50741d7646e31b75dd0
BimaWijaya69 <catfly765@gmail.com>
2025-07-25 16:15:10 +0700

    first commit

---
+++ components/main.tsx
@@ -0,0 +1,18 @@
+import Card from "@/components/card";
+
+function Main() {
+  return (
+    <div className="max-w-screen-xl pb-20 px-4 py-8 mx-auto">
+      <div className="grid gap-7 md:grid-cols-3 ">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
+    </div>
+  );
+}
+
+export default Main;
