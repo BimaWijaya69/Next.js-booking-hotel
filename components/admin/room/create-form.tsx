49cb8fc6f19a8facbef3843652d5e293c5033d81
BimaWijaya69 <catfly765@gmail.com>
2025-07-31 23:02:11 +0700

    read data room

--- components/admin/room/create-form.tsx
+++ components/admin/room/create-form.tsx
@@ -1,10 +1,13 @@
 "use client";
 import { useRef, useState, useTransition } from "react";
+import { useActionState } from "react";
+import { SaveRoom } from "@/lib/actions";
 import { IoCloudUploadOutline, IoTrashOutline } from "react-icons/io5";
 import { type PutBlobResult } from "@vercel/blob";
 import Image from "next/image";
 import { BarLoader } from "react-spinners";
 import { Amenities } from "@prisma/client";
+import clsx from "clsx";
 
 const CreateForm = ({ amenities }: { amenities: Amenities[] }) => {
   const inputFileRef = useRef<HTMLInputElement>(null);
@@ -48,8 +51,13 @@ const CreateForm = ({ amenities }: { amenities: Amenities[] }) => {
     });
   };
 
+  const [state, formAction, isPending] = useActionState(
+    SaveRoom.bind(null, image),
+    null
+  );
+
   return (
-    <form action="">
+    <form action={formAction}>
       <div className="grid md:grid-cols-12 gap-5">
         <div className="col-span-8 bg-white p-4">
           <div className="mb-4">
@@ -60,7 +68,9 @@ const CreateForm = ({ amenities }: { amenities: Amenities[] }) => {
               placeholder="Room Name"
             />
             <div aria-live="polite" aria-atomic="true">
-              <span className="text-sm text-red-500 mt-2">message</span>
+              <span className="text-sm text-red-500 mt-2">
+                {state?.error?.name}
+              </span>
             </div>
           </div>
           <div className="mb-4">
@@ -71,7 +81,9 @@ const CreateForm = ({ amenities }: { amenities: Amenities[] }) => {
               placeholder="Description"
             ></textarea>
             <div aria-live="polite" aria-atomic="true">
-              <span className="text-sm text-red-500 mt-2">message</span>
+              <span className="text-sm text-red-500 mt-2">
+                {state?.error?.description}
+              </span>
             </div>
           </div>
           <div className="mb-4 grid md:grid-cols-3">
@@ -89,7 +101,9 @@ const CreateForm = ({ amenities }: { amenities: Amenities[] }) => {
               </div>
             ))}
             <div aria-live="polite" aria-atomic="true">
-              <span className="text-sm text-red-500 mt-2">message</span>
+              <span className="text-sm text-red-500 mt-2">
+                {state?.error?.amenities}
+              </span>
             </div>
           </div>
         </div>
@@ -149,7 +163,9 @@ const CreateForm = ({ amenities }: { amenities: Amenities[] }) => {
               placeholder="Capacity (Person)"
             />
             <div aria-live="polite" aria-atomic="true">
-              <span className="text-sm text-red-500 mt-2">message</span>
+              <span className="text-sm text-red-500 mt-2">
+                {state?.error?.capacity}
+              </span>
             </div>
           </div>
           <div className="mb-4">
@@ -160,14 +176,30 @@ const CreateForm = ({ amenities }: { amenities: Amenities[] }) => {
               placeholder="Price (IDR)"
             />
             <div aria-live="polite" aria-atomic="true">
-              <span className="text-sm text-red-500 mt-2">message</span>
+              <span className="text-sm text-red-500 mt-2">
+                {state?.error?.price}
+              </span>
             </div>
           </div>
+          {/* General Message */}
+          {state?.message ? (
+            <div className="mb-4 bg-red-200 p-2">
+              <span className="text-sm text-gray-700 mt-2">
+                {state.message}
+              </span>
+            </div>
+          ) : null}
           <button
             type="submit"
-            className="bg-orange-400 text-white w-full hover:bg-orange-500 py-2.5 px-6 md:px-10 text-lg font-semibold cursor-pointer"
+            className={clsx(
+              "bg-orange-400 text-white w-full hover:bg-orange-500 py-2.5 px-6 md:px-10 text-lg font-semibold cursor-pointer",
+              {
+                "opacity-50 cursor-progress": isPending || pending,
+              }
+            )}
+            disabled={isPending || pending}
           >
-            Save
+            {isPending ? "Saving..." : "Save Room"}
           </button>
         </div>
       </div>
--- lib/actions.ts
+++ lib/actions.ts
@@ -1,6 +1,51 @@
 "use server";
-import { ContactSchema } from "@/lib/zod";
+import { ContactSchema, RoomSchema } from "@/lib/zod";
 import { prisma } from "@/lib/prisma";
+import { redirect } from "next/navigation";
+
+export const SaveRoom = async (
+  image: string,
+  prevState: unknown,
+  formData: FormData
+) => {
+  if (!image) return { message: "Image is required." };
+
+  const rawData = {
+    name: formData.get("name"),
+    description: formData.get("description"),
+    capacity: formData.get("capacity"),
+    price: formData.get("price"),
+    amenities: formData.getAll("amenities"),
+  };
+
+  const validateFields = RoomSchema.safeParse(rawData);
+  if (!validateFields.success) {
+    return { error: validateFields.error.flatten().fieldErrors };
+  }
+  const { name, description, capacity, price, amenities } = validateFields.data;
+
+  try {
+    await prisma.room.create({
+      data: {
+        name,
+        description,
+        capacity,
+        price,
+        image,
+        RoomAmenities: {
+          createMany: {
+            data: amenities.map((item) => ({
+              amenitiesId: item,
+            })),
+          },
+        },
+      },
+    });
+  } catch (error) {
+    console.log(error);
+  }
+  redirect("/admin/room");
+};
 
 export const ContactMessage = async (
   prevState: unknown,
